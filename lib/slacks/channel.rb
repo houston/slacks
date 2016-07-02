5a1cd8fcc574d66417ca793a7b53a53bc2c08909
Bob Lail <bob.lailfamily@gmail.com>
2016-07-02 11:34:52 -0500

    [skip] Removed Channel#random_reply (1m)
    Moved this feature to Houston::Conversations

--- lib/slacks/channel.rb
+++ lib/slacks/channel.rb
@@ -27,26 +27,6 @@ module Slacks
     end
     alias :say :reply
 
-    def random_reply(replies)
-      if replies.is_a?(Hash)
-        weights = replies.values
-        unless weights.reduce(&:+) == 1.0
-          raise ArgumentError, "Reply weights don't add up to 1.0"
-        end
-
-        draw = rand
-        sum = 0
-        pick = nil
-        replies.each do |reply, weight|
-          pick = reply unless sum > draw
-          sum += weight
-        end
-        reply pick
-      else
-        reply replies.sample
-      end
-    end
-
     def typing
       slack.typing_on(self)
     end
