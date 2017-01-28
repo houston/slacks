5333369bde67499ed3fafec35631eb43593554d8
Bob Lail <bob.lailfamily@gmail.com>
2017-01-28 11:16:33 -0600

    [skip] Deleted unused error (1m)

--- lib/slacks/errors.rb
+++ lib/slacks/errors.rb
@@ -24,12 +24,6 @@ module Slacks
     end
   end
 
-  class AlreadyRespondedError < RuntimeError
-    def initialize(message=nil)
-      super message || "You have already replied to this Slash Command; you can only reply once"
-    end
-  end
-
   class NotInChannelError < RuntimeError
     def initialize(channel)
       super "The bot is not in the channel #{channel} and cannot reply"
